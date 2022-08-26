17c9355bc8a9d0a0de29d9289dccce778e44f3e3
Roman Burko <redstone.kem@gmail.com>
2022-08-26 18:15:52 +0300

    refactor: shift logic from binary to main file

--- src/comparison.php
+++ src/comparison.php
@@ -2,6 +2,37 @@
 
 namespace Gendiff\Comparison;
 
+use Docopt;
+
+const DOC = <<<D
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
+
+D;
+
+function getFullPath($path)
+{
+    $addedPart = $path[0] === '/' ? '' : __DIR__ . "/../";
+    return $addedPart . $path;
+}
+
+function run() {
+    $args = Docopt::handle(DOC);
+    $pathToFile1 = getFullPath($args['<firstFile>']);
+    $pathToFile2 = getFullPath($args['<secondFile>']);
+    $format = $args['--format'];
+    gendiff($pathToFile1, $pathToFile2, $format);
+}
+
 function readFile($path)
 {
     return file_get_contents($path, true);
