05a63e91a57f1e2a21f0556caa319a9ba341648b
Roman Burko <redstone.kem@gmail.com>
2022-09-02 15:41:22 +0300

    refactor: internal representation of diff

--- src/Comparison.php
+++ src/Comparison.php
@@ -5,6 +5,14 @@ namespace Gendiff\Comparison;
 use function Gendiff\Parsers\parseJson;
 use function Gendiff\Parsers\parseYaml;
 
+const OPERATION_SIGNS = [
+    'added' => '+',
+    'removed' => '-',
+    'unchanged' => ' ',
+    'changed' => ' ',
+    'updated' => ['-', '+']
+];
+
 function readFile($path)
 {
     return file_get_contents($path, true);
@@ -33,7 +41,7 @@ function gendiff($pathToFile1, $pathToFile2, $format)
     $file1 = prepareFileToComparison($pathToFile1);
     $file2 = prepareFileToComparison($pathToFile2);
     $diff = makeDiff($file1, $file2);
-    $diffToPrint = stringifyDiff($diff);
+    $diffToPrint = formatDiffStylish($diff);
     print $diffToPrint;
 }
 
@@ -46,17 +54,39 @@ function isAssociativeArray($array)
     return $array !== $filtered;
 }
 
-function makeStructureRec($key, $value, $sign = ' ')
+function stringifyIfIndexArray($var)
 {
-    $result = !isAssociativeArray($value) ?
-        toString($value) :
-        array_map(fn($newKey, $newValue) => makeStructureRec($newKey, $newValue), array_keys($value), $value);
-    return ['sign' => $sign, 'key' => $key, 'value' => $result];
+    if (!is_array($var) || isAssociativeArray($var)) {
+        return $var;
+    }
+
+    $iter = function ($value) use (&$iter) {
+        if (!is_array($value)) {
+            return trim(var_export($value, true), "'");
+        }
+
+        $lines = array_map(fn($item) => $iter($item), $value);
+        return "[" . implode(", ", $lines) . "]";
+    };
+
+    return $iter($var);
 }
 
-function makeStructureIter($key, $value, $sign = ' ')
+function makeStructureRec($key, $value1, $value2 = null, $diff = 'unchanged')
 {
-    return ['sign' => $sign, 'key' => $key, 'value' => $value];
+    $result1 = isAssociativeArray($value1) ?
+        array_map(fn($newKey, $newValue) => makeStructureRec($newKey, $newValue), array_keys($value1), $value1) :
+        stringifyIfIndexArray($value1);
+
+    $result2 = isAssociativeArray($value2) ?
+        array_map(fn($newKey, $newValue) => makeStructureRec($newKey, $newValue), array_keys($value2), $value2) :
+        stringifyIfIndexArray($value2);
+    return ['key' => $key, 'value1' => $result1, 'value2' => $result2, 'diff' => $diff];
+}
+
+function makeStructureIter($key, $value1, $value2 = null, $diff = 'changed')
+{
+    return ['key' => $key, 'value1' => $value1, 'value2' => $value2, 'diff' => $diff];
 }
 
 function makeDiff($file1, $file2)
@@ -67,68 +97,76 @@ function makeDiff($file1, $file2)
         $keys = array_unique(array_merge($keys1, $keys2));
         sort($keys);
 
-        $callback = function ($acc, $key) use ($iter, $file1, $file2) {
+        $callback = function ($key) use ($iter, $file1, $file2) {
             $value1 = $file1[$key] ?? null;
             $value2 = $file2[$key] ?? null;
 
             if (!array_key_exists($key, $file1)) {
-                return [...$acc, makeStructureRec($key, $value2, '+')];
+                return makeStructureRec($key, $value2, null, 'added');
             }
 
             if (!array_key_exists($key, $file2)) {
-                return [...$acc, makeStructureRec($key, $value1, '-')];
+                return makeStructureRec($key, $value1, null, 'removed');
             }
 
             if ($value1 === $value2) {
-                return [...$acc, makeStructureRec($key, $value1, ' ')];
+                return makeStructureRec($key, $value1, null, 'unchanged');
             }
 
             if (!isAssociativeArray($value1) || !isAssociativeArray($value2)) {
-                return [...$acc, makeStructureRec($key, $value1, '-'), makeStructureRec($key, $value2, '+')];
+                return makeStructureRec($key, $value1, $value2, 'updated');
             }
 
-            return [...$acc, makeStructureIter($key, $iter($value1, $value2), ' ')];
+            return makeStructureIter($key, $iter($value1, $value2), null,'changed');
         };
 
-        return array_reduce($keys, $callback, []);
+        return array_map($callback, $keys);
     };
 
     return makeStructureIter('', $iter($file1, $file2));
 }
 
-function toString($var)
+function toStringStylish($value)
 {
-    $iter = function ($value) use (&$iter) {
-        if (!is_array($value)) {
-            return is_null($value) ? "null" : trim(var_export($value, true), "'");
-        }
-
-        $lines = array_map(function ($item) use ($iter) {
-            return $iter($item);
-        }, $value);
-        return "[" . implode(", ", $lines) . "]";
-    };
-
-    return $iter($var);
+    return is_null($value) ? "null" : trim(var_export($value, true), "'");
 }
 
-function stringifyDiff($diff)
+function formatDiffStylish($diff)
 {
-    $iter = function ($currentValue, $depth) use (&$iter) {
-        $children = $currentValue['value'];
+    $iter = function ($currentValue, $typeOfValue, $depth) use (&$iter) {
+        $children = $currentValue[$typeOfValue];
         if (!is_array($children)) {
-            return $children;
+            return toStringStylish($children);
         }
 
         $indent = str_repeat('    ', $depth - 1);
-        $lines = array_map(function ($item) use ($iter, $indent, $depth) {
-            $value = $iter($item, $depth + 1);
-            $spaceBeforeValue = empty($value) ? "" : " ";
-            return "{$indent}  {$item['sign']} {$item['key']}:{$spaceBeforeValue}{$value}";
-        }, $children);
 
+        $callback = function ($acc, $item) use ($iter, $indent, $depth) {
+            $key = $item['key'];
+            $difference = $item['diff'];
+
+            $value1 = $iter($item, 'value1', $depth + 1);
+            $spaceBeforeValue1 = empty($value1) ? "" : " ";
+
+            if ($difference !== 'updated') {
+                $sign = OPERATION_SIGNS[$difference];
+                return [...$acc, "{$indent}  {$sign} {$key}:{$spaceBeforeValue1}{$value1}"];
+            }
+
+            $value2 = $iter($item, 'value2', $depth + 1);
+            $spaceBeforeValue2 = empty($value2) ? "" : " ";
+            [$sign1, $sign2] = OPERATION_SIGNS[$difference];
+            return [
+                ...$acc,
+                "{$indent}  {$sign1} {$key}:{$spaceBeforeValue1}{$value1}",
+                "{$indent}  {$sign2} {$key}:{$spaceBeforeValue2}{$value2}"
+            ];
+        };
+
+        $lines = array_reduce($children, $callback, []);
         return "{\n" . implode("\n", $lines) . "\n" . $indent . "}";
+
     };
 
-    return $iter($diff, 1) . "\n";
+    return $iter($diff, 'value1', 1) . "\n";
 }
