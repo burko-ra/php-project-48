46387120a9f6c45772d897413bc311fc6b16c793
Roman Burko <redstone.kem@gmail.com>
2022-09-12 12:06:16 +0300

    refactor: Json.php

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -8,64 +8,12 @@ use function Differ\Diff\getValue2;
 use function Differ\Diff\getOperation;
 
 /**
- * @param mixed $value
- * @return mixed
- */
-function makeAssociativeArray($value)
-{
-    if (!is_array($value)) {
-        return $value;
-    }
-    return array_reduce($value, function ($acc, $item) {
-        $newKey = getKey($item);
-        $newValue = makeAssociativeArray(getValue1($item));
-        return array_merge($acc, [$newKey => $newValue]);
-    }, []);
-}
-
-/**
- * @param string $property
- * @param array<mixed> $element
- * @return array<mixed>
- */
-function makeStructure(string $property, array $element): array
-{
-    $operation = getOperation($element);
-    $value1 = getValue1($element);
-    $value2 = getValue2($element);
-
-    return [$property => [
-        'operation' => $operation,
-        'value1' => makeAssociativeArray($value1),
-        'value2' => makeAssociativeArray($value2)
-        ]
-    ];
-}
-
-/**
- * @param array<mixed> $operation
+ * @param array<mixed> $diff
  * @return string
  */
-function formatDiffJson(array $operation): string
+function formatDiffJson(array $diff): string
 {
-    $iter = function ($currentValue, $currentPath, $depth, $acc) use (&$iter) {
-        $property = $currentPath . getKey($currentValue);
-        $operation = getOperation($currentValue);
-
-        if ($operation === 'added' || $operation === 'removed' || $operation === 'updated') {
-            return array_merge($acc, makeStructure($property, $currentValue));
-        }
-
-        if ($operation === 'changed') {
-            $children = getValue1($currentValue);
-            $newPath = ($depth === 1) ? $property : "{$property}.";
-            return array_reduce($children, fn($newAcc, $item) => $iter($item, $newPath, $depth + 1, $newAcc), $acc);
-        }
-
-        return $acc;
-    };
-
-    $encoded = json_encode($iter($operation, '', 1, []), JSON_PRETTY_PRINT);
+    $encoded = json_encode($diff, JSON_PRETTY_PRINT);
     if ($encoded === false) {
         throw new \Exception("Invalid diff. Cannot encode to json");
     }
