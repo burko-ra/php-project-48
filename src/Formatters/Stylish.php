0f682855846b399a994cd222fa0e39141cf36843
Roman Burko <redstone.kem@gmail.com>
2022-09-16 15:15:29 +0300

    refactor: simplified formatDiffStylish function

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -3,7 +3,8 @@
 namespace Differ\Formatters\Stylish;
 
 use function Differ\Diff\getKey;
-use function Differ\Diff\getValue;
+use function Differ\Diff\getValue1;
+use function Differ\Diff\getValue2;
 use function Differ\Diff\getOperation;
 
 /**
@@ -16,48 +17,53 @@ function toStringStylish($value): string
 }
 
 /**
+ * @param array<mixed> $currentValue
+ * @param int $depth
+ * @return string
+ */
+function makeStylish($currentValue, int $depth): string
+{
+    if (!is_array($currentValue)) {
+        return toStringStylish($currentValue);
+    }
+
+    $indent = str_repeat('    ', $depth - 1);
+
+    $callback = function ($acc, $item) use ($indent, $depth) {
+        $key = getKey($item);
+        $operation = getOperation($item);
+
+        $value1 = makeStylish(getValue1($item), $depth + 1);
+        $value2 = makeStylish(getValue2($item), $depth + 1);
+
+        if ($operation === 'added') {
+            return [...$acc, "{$indent}  + {$key}: {$value1}"];
+        }
+
+        if ($operation === 'removed') {
+            return [...$acc, "{$indent}  - {$key}: {$value1}"];
+        }
+
+        if ($operation === 'updated') {
+            return [
+                ...$acc,
+                "{$indent}  - {$key}: {$value1}",
+                "{$indent}  + {$key}: {$value2}"
+            ];
+        }
+
+        return [...$acc, "{$indent}    {$key}: {$value1}"];
+    };
+
+    $lines = array_reduce($currentValue, $callback, []);
+    return "{\n" . implode("\n", $lines) . "\n" . $indent . "}";
+}
+
+/**
  * @param array<mixed> $diff
  * @return string
  */
 function formatDiffStylish(array $diff): string
 {
-    $iter = function ($currentValue, $typeOfValue, $depth) use (&$iter) {
-        $children = getValue($currentValue, $typeOfValue);
-        if (!is_array($children)) {
-            return toStringStylish($children);
-        }
-
-        $indent = str_repeat('    ', $depth - 1);
-
-        $callback = function ($acc, $item) use ($iter, $indent, $depth) {
-            $key = getKey($item);
-            $operation = getOperation($item);
-
-            $value1 = $iter($item, 'value1', $depth + 1);
-            $value2 = $iter($item, 'value2', $depth + 1);
-
-            if ($operation === 'added') {
-                return [...$acc, "{$indent}  + {$key}: {$value1}"];
-            }
-
-            if ($operation === 'removed') {
-                return [...$acc, "{$indent}  - {$key}: {$value1}"];
-            }
-
-            if ($operation === 'updated') {
-                return [
-                    ...$acc,
-                    "{$indent}  - {$key}: {$value1}",
-                    "{$indent}  + {$key}: {$value2}"
-                ];
-            }
-
-            return [...$acc, "{$indent}    {$key}: {$value1}"];
-        };
-
-        $lines = array_reduce($children, $callback, []);
-        return "{\n" . implode("\n", $lines) . "\n" . $indent . "}";
-    };
-
-    return $iter($diff, 'value1', 1);
+    return makeStylish(getValue1($diff), 1);
 }
